6959f030afaa09e8be19bd6fb9fb2a016659410b
ibarretom <igorbm.23@gmail.com>
2021-12-28 02:02:18 -0300

    Componente SignInButton

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,21 @@
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+
+import styles from "./styles.module.scss";
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      Meu nome
+      <FiX color="#737380" className={styles.closedIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417" />
+      Sign in with Github
+    </button>
+  );
+}
