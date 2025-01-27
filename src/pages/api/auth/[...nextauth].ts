a5fa7cb8f4ac10b04716d6f804ff40bc1f550bb5
igorbmeirelles <igorbm.23@gmail.com>
2025-01-27 00:54:32 -0300

    fix: link and authentication

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -17,6 +17,9 @@ export default NextAuth({
       },
     }),
   ],
+  jwt: {
+    secret: "teste",
+  },
   callbacks: {
     async session({ session }) {
       try {
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -34,7 +34,7 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <Link key={post.slug} href={`/posts/${post.slug}`}>
+            <Link key={post.slug} href={`/posts/${post.slug}`} legacyBehavior>
               <a>
                 <time>{post.updatedAt}</time>
                 <strong>{post.title}</strong>
